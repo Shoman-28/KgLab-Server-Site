18bdbd097c42412fd0e4fdebdb11ce77abac4427
shoman <northstar9928@gmail.com>
2022-06-18 14:05:49 +0600

    add

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.connect(err => {
   const addAppointmnetCollection = client.db("doctors").collection("addAppointments");
   const doctorCollection = client.db("doctors").collection("doctorDetails");
   const adddoctorCollection = client.db("doctors").collection("addDoctor");
-  
+
   //   //Post Add Appointment
   app.post("/addAppointmnet", (req, res) => {
     addAppointmnetCollection.insertOne(req.body)    
@@ -112,102 +112,6 @@ client.connect(err => {
 
 
 });
-
-
-
-// app.listen(port, () => {
-//   console.log("successfully digital server site");
-// });
-// app.listen(process.env.PORT || port)
-// app.listen(process.env.PORT ||port);
-  
-// });
-
-
-// client.connect((err) => {
-//   const addAppointmnetCollection = client.db("doctors").collection("addAppointments");
-//   const doctorCollection = client.db("doctors").collection("doctorDetails");
-//   const adddoctorCollection = client.db("doctors").collection("addDoctor");
-
-
-//   //Post Add Appointment
-//   app.post("/addAppointmnet", (req, res) => {
-//     addAppointmnetCollection.insertOne(req.body)    
-//       .then((result) => {
-//         res.send(result.insertedCount > 0);
-//       })
-//       .catch((err) => {
-//         console.log(err, "error message");
-//       });
-//   });
-
-  
-//   //Get All Appointment
-//   app.get("/allAppointments", (req, res) => {
-//     addAppointmnetCollection.find({})
-//     .toArray((err, documents)=>{
-//       res.send(documents);
-//     })
-    
-//   });
-
-
-//    //Post Add Doctore
-//   app.post('/addADoctore',  (req, res) =>{    
-//     doctorCollection.insertOne(req.body)    
-//       .then(result=>{
-//       res.send(result.insertedCount > 0)
-//       })
-//       .catch(err=>{
-//         console.log(err, "error message")
-//       })
-      
-//     })
-//  //Get All Appointment
-//  app.get("/allDoctor", (req, res) => {
-//   console.log(res)
-//   doctorCollection.find({})
-//   .toArray((err, documents)=>{
-//     res.send(documents);
-//   })
-  
-// });
-
-
- 
-
-
-//   app.post('/appointmentsByDate', (req, res)=>{
-//       const date = req.body;
-//       const email = req.body.email;      
-//       doctorCollection.find({email: email})
-//         .toArray((err, doctors)=>{
-//             const filter = {date: date.date}
-//             if(doctors.length === 0){
-//               filter.email = email;
-//             }
-//             addAppointmnetCollection.find(filter)
-//             .toArray((err, documents)=>{
-//               console.log(email, date.date, doctors, documents)
-//                 res.send(documents);
-//             })
-//         })
-     
-//   })
-
-//   app.post('/checkAdmin', (req, res)=>{
-//     doctorCollection.find({email:req.body.email})
-//     .toArray((err, documents)=>{
-//       res.send(documents)          
-      
-//     })
-    
-//   })
-
-  
-
-
-// });
 
 
 
